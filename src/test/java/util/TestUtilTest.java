0b14343c7be960ffdd57de3d38be9a0d4deb71dc
이건희 <dnzp74@g.eulji.ac.kr>
2024-11-28 17:57:58 +0900

    test: TestUtil.setOutToByteArray() - RED

--- src/test/java/util/TestUtilTest.java
+++ src/test/java/util/TestUtilTest.java
@@ -33,7 +33,19 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("나의 죽음을 적들에게 알리지 말라!");
         assertThat(author).isEqualTo("이순신");
+    }
 
+    @Test
+    @DisplayName("TestUtil.setOutToByteArray()- testUtil을 만드는 테스트")
+    public void t2(){
+        Scanner byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+        String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        assertThat(out).isEqualTo("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
+        System.out.println("이제는 화면에 출력됩니다.");
 
     }
 
