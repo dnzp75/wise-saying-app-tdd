7351c05cc1b5a82750285c14b329f1d8e8d8e814
이건희 <dnzp74@g.eulji.ac.kr>
2024-11-28 16:57:10 +0900

    feat: 선 입력된 명령어를 자동으로 입력받는 코드 작성

--- src/main/java/com/WearWeather/wear/Main.java
+++ src/main/java/com/WearWeather/wear/Main.java
@@ -1,5 +1,7 @@
 package com.WearWeather.wear;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
@@ -7,14 +9,31 @@ public class Main {
     public static void main(String[] args) {
        // 사람이 직접 테스트 하는 코드
         lab1();
+
+        lab2();
     }
 
     private static void lab1() {
+        // 사람이 키보드로 입력한 System.in을 입력받음
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("명령");
         String cmd = scanner.nextLine().trim();
 
         System.out.println("입력한 명령: " + cmd);
+    }
+
+    private static void lab2() {
+        // 메롱이 선 입력된 in을 자동으로 입력받는다.
+        InputStream in = new ByteArrayInputStream("메롱".getBytes());
+
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("명령");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령: " + cmd);
+
+
     }
 }
